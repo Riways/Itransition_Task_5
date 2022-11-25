679cad90c0f5eae40bbd32e4178ac6c5f84e2f12
Viktar Baradzin <a0fragile@gmail.com>
2022-11-25 18:58:31 +0300

    V1

---
+++ Models/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace  Task_5.Models
+{
+    public class PersonModel
+    {
+        public int Num { get; set; }
+        public string Id { get; set; }
+        public string Fullname { get; set; }
+        public string FullAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override string? ToString()
+        {
+            return string.Join(", ", Id, Fullname, FullAddress, PhoneNumber);
+        }
+    }
+
+    
+}
---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,18 @@
+﻿/*    $.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    $("#scrollWindow").on('scroll', function() {
+        DotNet.invokeMethodAsync('BlazorSample', 'AddPerson')
+            .then(data => {
+                console.log(data);
+            });
+    });
+
+*/
